8e2b5bdc345387269bb4dc5184c180b5b0cb2b77
Tom Reinert <tom.reinert@fondof.de>
2020-09-15 11:37:50 +0200

    switch to data file for navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,25 @@
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy('images')
+  eleventyConfig.addPassthroughCopy('admin')
+
+  const {
+    DateTime
+  } = require("luxon");
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+      return DateTime.fromJSDate(dateObj, {
+        zone: 'utc'
+      }).toFormat('yy-MM-dd');
+    });
+
+    eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: 'utc'
+    }).toFormat("dd-MM-yy");
+  });
+
   return {
     dir: { input: 'src', output: '_site' }
   };
