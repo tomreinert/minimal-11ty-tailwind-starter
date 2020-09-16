c0e2c8ec1ff657614e9bd32324bc9516603aae70
Tom Reinert <tom.reinert@fondof.de>
2020-09-16 11:44:31 +0200

    add navigation with 11ty navigation plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   eleventyConfig.addPassthroughCopy('images')
   eleventyConfig.addPassthroughCopy('admin')
