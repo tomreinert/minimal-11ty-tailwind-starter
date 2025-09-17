54ee69dae82d979afa1924701b90642315504a41
Tom Reinert <tom.reinert@koeln.de>
2025-09-17 14:26:45 +0200

    upgrade

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,16 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const tailwindcss = require('eleventy-plugin-tailwindcss-4')
 
 
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPassthroughCopy('src/img')
+
+  eleventyConfig.addPlugin(tailwindcss, {
+    input: 'styles/main.css',
+    output: 'assets/main.css'
+  } );
 
 
   const {
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  darkMode: 'class',
-  content: [
-    "./src/_includes/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
-    "./src/blog/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
-    "./src/pages/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
-    "./src/index.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
-  ]
-}
